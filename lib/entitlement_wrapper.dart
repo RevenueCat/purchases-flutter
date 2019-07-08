7e177bfb56c798cca07b13ec36281533ffadc341
Cesar de la Vega <cesarvegaro@gmail.com>
2019-07-08 12:47:44 -0700

    adds more docs

--- lib/entitlement_wrapper.dart
+++ lib/entitlement_wrapper.dart
@@ -1,6 +1,7 @@
 import 'object_wrappers.dart';
 
 class Entitlement {
+  // Map of offerings to their active product for the current platform
   final Map<String, Product> offerings;
 
   Entitlement.fromJson(Map<dynamic, dynamic> json)
