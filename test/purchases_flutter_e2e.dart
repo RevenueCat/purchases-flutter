2981196130f95c8f68c1be05c7cc24fbc31c2181
Cesar de la Vega <cesarvegaro@gmail.com>
2020-06-26 13:01:34 -0700

    adds comment

--- test/purchases_flutter_e2e.dart
+++ test/purchases_flutter_e2e.dart
@@ -2,6 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:e2e/e2e.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 
+// Execute the following in the `example` folder run these tests:
+// `flutter drive --driver=test_driver/purchases_flutter_e2e_test.dart ../test/purchases_flutter_e2e.dart`
+
 void main() {
   E2EWidgetsFlutterBinding.ensureInitialized();
 
