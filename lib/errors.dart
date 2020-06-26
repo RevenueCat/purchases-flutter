784e12d94d1c38d35f8e628dc238aadb04ba2ea7
Cesar de la Vega <cesarvegaro@gmail.com>
2020-06-26 14:28:07 -0700

    improves errors

--- lib/errors.dart
+++ lib/errors.dart
@@ -25,8 +25,31 @@ enum PurchasesErrorCode {
   invalidSubscriberAttributesError
 }
 
+/// Helper to convert from PlatformExceptions to PurchasesErrorCodes
 class PurchasesErrorHelper {
+
+  /// Use this to convert a PlatformException to a PurchasesErrorCode.
+  /// It will return `PurchasesErrorCode.unknownError` if the error code is not
+  /// in the range of PurchasesErrorCodes.
+  ///
+  /// ```
+  /// try {
+  ///    PurchaserInfo purchaserInfo = await Purchases.purchasePackage(package);
+  /// } on PlatformException catch (e) {
+  ///     var errorCode = PurchasesErrorHelper.getErrorCode(e);
+  ///     if (errorCode == PurchasesErrorCode.purchaseCancelledError) {
+  ///        print("User cancelled");
+  ///     } else if (errorCode == PurchasesErrorCode.purchaseNotAllowedError) {
+  ///        print("User not allowed to purchase");
+  ///     }
+  /// }
+  /// ```
   static PurchasesErrorCode getErrorCode(PlatformException e) {
-    return PurchasesErrorCode.values[int.parse(e.code)];
+    var errorCode = int.parse(e.code);
+    if (errorCode >= PurchasesErrorCode.values.length) {
+      return PurchasesErrorCode.unknownError;
+    }
+    return PurchasesErrorCode.values[errorCode];
   }
+
 }
