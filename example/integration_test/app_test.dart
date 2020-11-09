71b8b8af0019776c65f927d5169c045edd36ee61
Cesar de la Vega <cesarvegaro@gmail.com>
2020-11-09 11:55:33 -0800

    catches exceptions in tests

--- example/integration_test/app_test.dart
+++ example/integration_test/app_test.dart
@@ -1,4 +1,5 @@
 // Imports the Flutter Driver API.
+import 'package:flutter/services.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -6,16 +7,25 @@ import 'package:integration_test/integration_test.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-    setUpAll(() async {
-      await Purchases.setup("api_key", appUserId: "test-user");
-    });
-
-  test("Configures without crashing", () async {
-    expect(Purchases.appUserID != null, true);
+  setUpAll(() async {
+    await Purchases.setup("api_key", appUserId: "test-user");
   });
 
-  test("Purchaser info fetched successfully", () async {
-    PurchaserInfo purchaserInfo = await Purchases.getPurchaserInfo();
-    expect(purchaserInfo != null, false);
+  testWidgets("Configures without crashing", (WidgetTester tester) async {
+    try {
+      expect(Purchases.appUserID != null, true);
+    } on PlatformException catch (e) {
+      fail("there was an exception " + e.toString());
+    }
+  });
+
+  testWidgets("Purchaser info fetched successfully",
+      (WidgetTester tester) async {
+    try {
+      PurchaserInfo purchaserInfo = await Purchases.getPurchaserInfo();
+      expect(purchaserInfo != null, true);
+    } on PlatformException catch (e) {
+      fail("there was an exception " + e.toString());
+    }
   });
 }
