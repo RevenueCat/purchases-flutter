cb518fde3f3cebb72ba6cd0f8e7ba6123592c48f
aboedo <andresboedo@gmail.com>
2021-05-05 17:39:52 -0300

    Release: 3.2.2 (#192)
    * bumped version number to 3.2.2, bumped purchases-hybrid-common to 1.6.3
    
    * update fastlane
    
    * updated Podfile.lock

--- android/src/main/java/com/revenuecat/purchases_flutter/PurchasesFlutterPlugin.java
+++ android/src/main/java/com/revenuecat/purchases_flutter/PurchasesFlutterPlugin.java
@@ -53,7 +53,7 @@ public class PurchasesFlutterPlugin implements FlutterPlugin, MethodCallHandler,
     @Nullable private Activity activity;
 
     private static final String PLATFORM_NAME = "flutter";
-    private static final String PLUGIN_VERSION = "3.2.1";
+    private static final String PLUGIN_VERSION = "3.2.2";
 
     /**
      * Plugin registration.
