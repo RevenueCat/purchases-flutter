5912d5c7ec59783b50de6787f925f56058fd116a
Dan Gilbert <ddg@daentech.co.uk>
2021-11-30 13:12:32 +0100

    Add tests for ownership type

--- test/entitlement_info_test.dart
+++ test/entitlement_info_test.dart
@@ -49,4 +49,68 @@ void main() {
 
     expect(entitlementInfo.store, Store.unknownStore);
   });
+
+  test('unknown ownership type if missing from json', () {
+    Map<dynamic, dynamic> entitlementInfoJson = {
+      "identifier": "almost_pro",
+      "isActive": true,
+      "willRenew": true,
+      "latestPurchaseDateMillis": 1.58759855E9,
+      "latestPurchaseDate": "2020-04-22T23:35:50.000Z",
+      "originalPurchaseDateMillis": 1.591725245E9,
+      "originalPurchaseDate": "2020-06-09T17:54:05.000Z",
+      "expirationDateMillis": null,
+      "expirationDate": null,
+      "store": "PLAY_STORE",
+      "productIdentifier": "consumable",
+      "isSandbox": true,
+      "unsubscribeDetectedAt": null,
+      "unsubscribeDetectedAtMillis": null,
+      "billingIssueDetectedAt": null,
+      "billingIssueDetectedAtMillis": null
+    };
+    final entitlementInfo = EntitlementInfo.fromJson(entitlementInfoJson);
+
+    expect(entitlementInfo.ownershipType, OwnershipType.unknown);
+  });
+
+  test('ownership type parsed from json', () {
+    Map<dynamic, dynamic> entitlementInfoJson = {
+      "identifier": "almost_pro",
+      "isActive": true,
+      "willRenew": true,
+      "latestPurchaseDateMillis": 1.58759855E9,
+      "latestPurchaseDate": "2020-04-22T23:35:50.000Z",
+      "originalPurchaseDateMillis": 1.591725245E9,
+      "originalPurchaseDate": "2020-06-09T17:54:05.000Z",
+      "expirationDateMillis": null,
+      "expirationDate": null,
+      "store": "PLAY_STORE",
+      "productIdentifier": "consumable",
+      "isSandbox": true,
+      "unsubscribeDetectedAt": null,
+      "unsubscribeDetectedAtMillis": null,
+      "billingIssueDetectedAt": null,
+      "billingIssueDetectedAtMillis": null
+    };
+    entitlementInfoJson["ownershipType"] = "PURCHASED";
+    var entitlementInfo = EntitlementInfo.fromJson(entitlementInfoJson);
+
+    expect(entitlementInfo.ownershipType, OwnershipType.purchased);
+
+    entitlementInfoJson["ownershipType"] = "FAMILY_SHARED";
+    entitlementInfo = EntitlementInfo.fromJson(entitlementInfoJson);
+
+    expect(entitlementInfo.ownershipType, OwnershipType.familyShared);
+
+    entitlementInfoJson["ownershipType"] = "UNKNOWN";
+    entitlementInfo = EntitlementInfo.fromJson(entitlementInfoJson);
+
+    expect(entitlementInfo.ownershipType, OwnershipType.unknown);
+
+    entitlementInfoJson["ownershipType"] = "AN_UNKNOWN_OWNERSHIP_TYPE";
+    entitlementInfo = EntitlementInfo.fromJson(entitlementInfoJson);
+
+    expect(entitlementInfo.ownershipType, OwnershipType.unknown);
+  });
 }
