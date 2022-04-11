02de9dc26c13b4f0de8a69eec0ca40c1b5345ed0
Maddie Beyl <maddie@revenuecat.com>
2022-04-11 10:35:21 -0700

    Add example to purchasetester

--- revenuecat_examples/purchase_tester/lib/main.dart
+++ revenuecat_examples/purchase_tester/lib/main.dart
@@ -35,6 +35,11 @@ class _MyAppState extends State<InitialScreen> {
 
     final purchaserInfo = await Purchases.getPurchaserInfo();
 
+    Purchases.addShouldPurchasePromoProductListener((makePurchase) async {
+      final purchaseResult = await makePurchase.call();
+      print(purchaseResult.purchaserInfo);
+    });
+
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
