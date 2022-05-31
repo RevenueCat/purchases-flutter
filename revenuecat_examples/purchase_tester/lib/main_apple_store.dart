f427b33a022928c7ae0b8a8d625037882f4be0eb
Cesar de la Vega <cesarvegaro@gmail.com>
2022-05-31 12:47:23 -0700

    renames flavor to store and uses constants

--- revenuecat_examples/purchase_tester/lib/main_apple_store.dart
+++ revenuecat_examples/purchase_tester/lib/main_apple_store.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:purchases_flutter_example/src/constant.dart';
 
 import 'flavor_config.dart';
 import 'src/app.dart';
 
 void main() {
-  FlavorConfig(
-    flavor: Flavor.appleStore,
-    //TO DO: add the API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
-    apiKey: "",
+  StoreConfig(
+    store: Store.appleStore,
+    apiKey: appleApiKey,
   );
 
   runApp(const PurchaseTester());
