b7608c4427a30f8032bc72c59ac2e79170d78aa2
Toni Rico <antonio.rico.diez@revenuecat.com>
2022-06-24 12:25:20 +0200

    Add api tests for all public methods and classes

---
+++ api_tester/lib/api_tests_import.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: unused_import
+import 'package:api_tester/errors_api_test.dart';
+import 'package:api_tester/models/customer_info_wrapper_api_test.dart';
+import 'package:api_tester/models/entitlement_info_wrapper_api_test.dart';
+import 'package:api_tester/models/entitlement_infos_wrapper_api_test.dart';
+import 'package:api_tester/models/introductory_price_api_test.dart';
+import 'package:api_tester/models/offering_wrapper_api_test.dart';
+import 'package:api_tester/models/offerings_wrapper_api_test.dart';
+import 'package:api_tester/models/package_wrapper_api_test.dart';
+import 'package:api_tester/models/promotional_offer_api_test.dart';
+import 'package:api_tester/models/purchase_configuration_api_test.dart';
+import 'package:api_tester/models/store_api_test.dart';
+import 'package:api_tester/models/store_product_discount_api_test.dart';
+import 'package:api_tester/models/store_product_wrapper_api_test.dart';
+import 'package:api_tester/models/store_transaction_api_test.dart';
+import 'package:api_tester/purchases_flutter_api_test.dart';
