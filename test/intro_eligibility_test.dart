0996a2bb400e1eea93007e5dde93c82987a6463e
Cesar de la Vega <cesarvegaro@gmail.com>
2022-07-14 12:18:48 +0200

    adds test

---
+++ test/intro_eligibility_test.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  const productName = 'monthly_intro_pricing_one_week';
+  const channel = MethodChannel('purchases_flutter');
+  final log = <MethodCall>[];
+  dynamic response;
+
+  setUp(() {
+    channel.setMockMethodCallHandler((call) async {
+      log.add(call);
+      return response;
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+    log.clear();
+    response = null;
+  });
+
+  test('checkTrialOrIntroductoryPriceEligibility returns eligibility map',
+      () async {
+    response = {
+      productName: {'status': 0, 'description': 'Unknown status'}
+    };
+    final list = await Purchases.checkTrialOrIntroductoryPriceEligibility(
+      [productName],
+    );
+    expect(list.length, 1);
+  });
+
+  test('introEligibilityStatusUnknown is returned', () async {
+    response = {
+      productName: {'status': 0, 'description': 'Unknown status'}
+    };
+    final list = await Purchases.checkTrialOrIntroductoryPriceEligibility(
+      [productName],
+    );
+    expect(
+      list[productName]?.status,
+      IntroEligibilityStatus.introEligibilityStatusUnknown,
+    );
+  });
+
+  test('introEligibilityStatusIneligible is returned', () async {
+    response = {
+      productName: {
+        'status': 1,
+        'description': 'Not eligible for trial or introductory price.'
+      }
+    };
+    final list = await Purchases.checkTrialOrIntroductoryPriceEligibility(
+      [productName],
+    );
+    expect(
+      list[productName]?.status,
+      IntroEligibilityStatus.introEligibilityStatusIneligible,
+    );
+  });
+
+  test('introEligibilityStatusEligible is returned', () async {
+    response = {
+      productName: {
+        'status': 2,
+        'description': 'Eligible for trial or introductory price.'
+      }
+    };
+    final list = await Purchases.checkTrialOrIntroductoryPriceEligibility(
+      [productName],
+    );
+    expect(
+      list[productName]?.status,
+      IntroEligibilityStatus.introEligibilityStatusEligible,
+    );
+  });
+
+  test('introEligibilityStatusNoIntroOfferExists is returned', () async {
+    response = {
+      productName: {
+        'status': 3,
+        'description': 'Product does not have trial or introductory price.'
+      }
+    };
+    final list = await Purchases.checkTrialOrIntroductoryPriceEligibility(
+      [productName],
+    );
+    expect(
+      list[productName]?.status,
+      IntroEligibilityStatus.introEligibilityStatusNoIntroOfferExists,
+    );
+  });
+}
