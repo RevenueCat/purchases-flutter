72b3807fb827d34296b4f0226e37ab497dff86a1
RCGitBot <dev+RCGitBot@revenuecat.com>
2022-11-10 12:50:50 +0000

    Preparing for next version

--- android/src/main/java/com/revenuecat/purchases_flutter/PurchasesFlutterPlugin.java
+++ android/src/main/java/com/revenuecat/purchases_flutter/PurchasesFlutterPlugin.java
@@ -51,7 +51,7 @@ public class PurchasesFlutterPlugin implements FlutterPlugin, MethodCallHandler,
     @Nullable private Activity activity;
 
     private static final String PLATFORM_NAME = "flutter";
-    private static final String PLUGIN_VERSION = "4.4.0";
+    private static final String PLUGIN_VERSION = "4.5.0-SNAPSHOT";
 
     /**
      * Plugin registration.
