2058b80f524bcc745ed047fe5a6a576d1d7c763c
Josh Holtz <me@joshholtz.com>
2023-04-10 20:56:08 -0500

    Forgot doc for iso8601

--- lib/models/period_wrapper.dart
+++ lib/models/period_wrapper.dart
@@ -14,7 +14,9 @@ class Period with _$Period {
     /// The increment of time that a subscription period is specified in
     @JsonKey(name: 'value') int value,
 
-    /// The increment of time that a subscription period is specified in
+    /// Specified in ISO 8601 format. For example, P1W equates to one week,
+    /// P1M equates to one month, P3M equates to three months, P6M equates to
+    /// six months, and P1Y equates to one year
     @JsonKey(name: 'iso8601') String iso8601,
   ) = _Period;
 
