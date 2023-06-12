b7ec061c2202471dfbd542d49bf7990a46ebb64d
RCGitBot <dev+RCGitBot@revenuecat.com>
2023-06-12 16:36:46 +0000

    Preparing for next version

--- android/src/main/java/com/revenuecat/purchases_flutter/PurchasesFlutterPlugin.java
+++ android/src/main/java/com/revenuecat/purchases_flutter/PurchasesFlutterPlugin.java
@@ -57,7 +57,7 @@ public class PurchasesFlutterPlugin implements FlutterPlugin, MethodCallHandler,
     private final Handler handler = new Handler(Looper.getMainLooper());
 
     private static final String PLATFORM_NAME = "flutter";
-    private static final String PLUGIN_VERSION = "5.0.0";
+    private static final String PLUGIN_VERSION = "5.1.0-SNAPSHOT";
 
     /**
      * Plugin registration.
