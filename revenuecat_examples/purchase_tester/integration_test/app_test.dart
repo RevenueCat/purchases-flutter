f3fdcda3bee25a2b95609ec0c9264864e228255b
Mark Villacampa <m@markvillacampa.com>
2023-08-03 00:33:54 +0200

    Use unique user id on each test run

--- revenuecat_examples/purchase_tester/integration_test/app_test.dart
+++ revenuecat_examples/purchase_tester/integration_test/app_test.dart
@@ -1,4 +1,5 @@
 // Imports the Flutter Driver API.
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -7,15 +8,17 @@ import 'package:purchases_flutter/purchases_flutter.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  var uuid = UniqueKey().toString();
+
   setUpAll(() {
     PurchasesConfiguration configuration = PurchasesConfiguration("appl_KhXKryBEHUWEdShrggQyjyzHKHW");
-    configuration.appUserID = "test-user";
+    configuration.appUserID = uuid;
     Purchases.configure(configuration);
   });
 
   testWidgets('Configures without crashing', (WidgetTester tester) async {
     try {
-      assert(await Purchases.appUserID == "test-user", true);
+      assert(await Purchases.appUserID == uuid, true);
     } on PlatformException catch (e) {
       fail('there was an exception ' + e.toString());
     }
@@ -24,7 +27,7 @@ void main() {
   testWidgets('Purchaser info fetched successfully',
       (WidgetTester tester) async {
     try {
-      assert((await Purchases.getCustomerInfo()).originalAppUserId == "test-user", true);
+      assert((await Purchases.getCustomerInfo()).originalAppUserId == uuid, true);
     } on PlatformException catch (e) {
       fail('there was an exception ' + e.toString());
     }
