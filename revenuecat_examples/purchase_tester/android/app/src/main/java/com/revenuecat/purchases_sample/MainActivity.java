96bf81669b6ee018b96e69043e269dee04885154
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-12-22 15:53:51 +0100

    Remove unused imports

--- revenuecat_examples/purchase_tester/android/app/src/main/java/com/revenuecat/purchases_sample/MainActivity.java
+++ revenuecat_examples/purchase_tester/android/app/src/main/java/com/revenuecat/purchases_sample/MainActivity.java
@@ -1,9 +1,5 @@
 package com.revenuecat.purchases_sample;
 
-import android.app.ActionBar;
-import android.os.Bundle;
-import android.view.Window;
-
 import io.flutter.embedding.android.FlutterFragmentActivity;
 
 public class MainActivity extends FlutterFragmentActivity {
