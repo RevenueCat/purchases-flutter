d45d24582f4922cfd48c35db6fce85ca795db0f6
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-01-03 11:38:59 +0100

    Update purchase tester to support new library

--- revenuecat_examples/purchase_tester/macos/Flutter/GeneratedPluginRegistrant.swift
+++ revenuecat_examples/purchase_tester/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import purchases_flutter
+import purchases_flutter_ui
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
+  PurchasesFlutterUiPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterUiPlugin"))
 }
