3d88dcc0b63b4f9cf4f46706bd811464b4524864
Toni Rico <antonio.rico.diez@revenuecat.com>
2024-02-20 12:24:39 +0100

    Remove unused import

--- revenuecat_examples/purchase_tester/integration_test/app_test.dart
+++ revenuecat_examples/purchase_tester/integration_test/app_test.dart
@@ -1,5 +1,4 @@
 // Imports the Flutter Driver API.
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
