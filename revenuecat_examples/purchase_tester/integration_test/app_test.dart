487d7852d1571822d528c245cabf850b99c967f8
Toni Rico <toni.rico.diez@revenuecat.com>
2024-02-20 13:35:15 +0100

    Fix ios integration tests (#985)
    ios integration tests were failing getting stuck in macos x86 machines.
    This updates to using M1 machines and updates to newer Xcode version,
    which required some small changes in the integration tests.

--- revenuecat_examples/purchase_tester/integration_test/app_test.dart
+++ revenuecat_examples/purchase_tester/integration_test/app_test.dart
@@ -1,5 +1,4 @@
 // Imports the Flutter Driver API.
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -8,18 +7,18 @@ import 'package:purchases_flutter/purchases_flutter.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  var uuid = UniqueKey().toString();
+  var userId = "integration-test-${DateTime.now().millisecondsSinceEpoch}";
 
   setUpAll(() {
     PurchasesConfiguration configuration = PurchasesConfiguration("appl_KhXKryBEHUWEdShrggQyjyzHKHW");
-    configuration.appUserID = uuid;
+    configuration.appUserID = userId;
     configuration.entitlementVerificationMode = EntitlementVerificationMode.informational;
     Purchases.configure(configuration);
   });
 
   testWidgets('Configures without crashing', (WidgetTester tester) async {
     try {
-      assert(await Purchases.appUserID == uuid, true);
+      assert(await Purchases.appUserID == userId, true);
     } on PlatformException catch (e) {
       fail('there was an exception ' + e.toString());
     }
@@ -28,7 +27,7 @@ void main() {
   testWidgets('Purchaser info fetched successfully',
       (WidgetTester tester) async {
     try {
-      assert((await Purchases.getCustomerInfo()).originalAppUserId == uuid, true);
+      assert((await Purchases.getCustomerInfo()).originalAppUserId == userId, true);
     } on PlatformException catch (e) {
       fail('there was an exception ' + e.toString());
     }
