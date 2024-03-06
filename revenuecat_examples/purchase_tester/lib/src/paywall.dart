f647a3f4875e9b72700014728ed421a5de40fe83
Cesar de la Vega <cesarvegaro@gmail.com>
2024-03-06 19:38:54 +0100

    add to sample

--- revenuecat_examples/purchase_tester/lib/src/paywall.dart
+++ revenuecat_examples/purchase_tester/lib/src/paywall.dart
@@ -12,7 +12,6 @@ class PaywallScreen extends StatefulWidget {
 }
 
 class _PaywallScreenState extends State<PaywallScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -21,11 +20,29 @@ class _PaywallScreenState extends State<PaywallScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: PaywallView(offering: widget.offering,),
+        body: SafeArea(
+      child: Center(
+        child: PaywallView(
+          offering: widget.offering,
+          onPurchaseStarted: (Package rcPackage) {
+            print('Purchase started for package: ${rcPackage.identifier}');
+          },
+          onPurchaseCompleted:
+              (CustomerInfo customerInfo, StoreTransaction storeTransaction) {
+            print('Purchase completed for customerInfo:\n $customerInfo\n '
+                'and storeTransaction:\n $storeTransaction');
+          },
+          onPurchaseError: (PurchasesError error) {
+            print('Purchase error: $error');
+          },
+          onRestoreCompleted: (CustomerInfo customerInfo) {
+            print('Restore completed for customerInfo:\n $customerInfo');
+          },
+          onRestoreError: (PurchasesError error) {
+            print('Restore error: $error');
+          },
         ),
       ),
-    );
+    ));
   }
 }
