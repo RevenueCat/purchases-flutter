9940c6f345a3c8cd7f51c2efac0650f7ae19b783
Josh Holtz <me@joshholtz.com>
2024-03-12 15:02:41 -0500

    revert constants

--- revenuecat_examples/purchase_tester/lib/src/constant.dart
+++ revenuecat_examples/purchase_tester/lib/src/constant.dart
@@ -1,8 +1,8 @@
 //TO DO: add the Apple API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
-const appleApiKey = 'appl_KFpRDwauDyfIGIETKyVymslMcjX';
+const appleApiKey = 'appl_api_key';
 
 //TO DO: add the Google API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
-const googleApiKey = 'goog_cRtaFpfwfcQYPXkIEqFUkVszUfN';
+const googleApiKey = 'googl_api_key';
 
 //TO DO: add the Amazon API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
 const amazonApiKey = 'amazon_api_key';
