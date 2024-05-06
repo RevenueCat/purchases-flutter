edbb5b0d5809d745bb14ec1b55c99d1ac0e9b308
Cesar de la Vega <cesarvegaro@gmail.com>
2024-05-06 15:27:03 +0200

    Migration to applying Gradle plugins with the declarative plugins block in sample (#1062)
    I kept getting 
    
    ```
    You are applying Flutter's main Gradle plugin imperatively using the apply script method, which is deprecated and will be removed in a future release. Migrate to applying Gradle plugins with the declarative plugins block: https://flutter.dev/go/flutter-gradle-plugin-apply
    ```
    
    when running the sample. I created a new plugin project and made some
    changes over to our purchase tester app

--- revenuecat_examples/purchase_tester/android/app/src/main/java/com/revenuecat/purchases_sample/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.revenuecat.purchases_sample;
-
-import io.flutter.embedding.android.FlutterFragmentActivity;
-
-public class MainActivity extends FlutterFragmentActivity {
-}
---
+++ revenuecat_examples/purchase_tester/android/app/src/main/java/com/revenuecat/purchases_sample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.revenuecat.purchases_sample
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity()
