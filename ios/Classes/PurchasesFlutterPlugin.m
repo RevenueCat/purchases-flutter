3368b467f9c2080f256c55111713721fde7ef2f4
RCGitBot <dev+RCGitBot@revenuecat.com>
2025-01-24 18:50:49 +0000

    Version bump for 8.4.3

--- ios/Classes/PurchasesFlutterPlugin.m
+++ ios/Classes/PurchasesFlutterPlugin.m
@@ -747,7 +747,7 @@ readyForPromotedProduct:(RCStoreProduct *)product
 }
 
 - (NSString *)platformFlavorVersion {
-    return @"8.4.2";
+    return @"8.4.3";
 }
 
 - (NSError *)createUnsupportedErrorWithDescription:(NSString *)description {
