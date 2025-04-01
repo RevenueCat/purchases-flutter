62275fec2d17192c1b20ae31e457c936932e1df4
Will Taylor <wtaylor151@gmail.com>
2025-04-01 12:29:28 -0500

    hard code ndk version

--- api_tester/android/app/build.gradle.kts
+++ api_tester/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.revenuecat.api_tester"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
