d9aaa87bdf940061b1f7de564dbff5934bfb688c
Will Taylor <wtaylor151@gmail.com>
2025-07-21 15:40:27 -0500

    add VirtualCurrency class + API test

---
+++ api_tester/lib/api_tests/models/virtual_currency_api_test.dart
@@ -0,0 +1,23 @@
+import 'package:purchases_flutter/object_wrappers.dart';
+
+// ignore_for_file: unused_element
+// ignore_for_file: unused_local_variable
+// ignore_for_file: deprecated_member_use
+class _VirtualCurrencyApiTest {
+  void _checkFromJsonFactory(Map<String, dynamic> json) {
+    VirtualCurrency vc = VirtualCurrency.fromJson(json);
+  }
+
+  void _checkConstructor(
+      int balance, String name, String code, String? serverDescription) {
+    VirtualCurrency vc =
+        VirtualCurrency(balance, name, code, serverDescription);
+  }
+
+  void _checkProperties(VirtualCurrency virtualCurrency) {
+    int balance = virtualCurrency.balance;
+    String name = virtualCurrency.name;
+    String code = virtualCurrency.code;
+    String? serverDescription = virtualCurrency.serverDescription;
+  }
+}
