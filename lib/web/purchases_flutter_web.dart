38d1a95324cedd92a0b4c2ee080f83605ceb80c4
RCGitBot <dev+RCGitBot@revenuecat.com>
2025-07-28 07:41:47 +0000

    Version bump for 16.1.0

--- lib/web/purchases_flutter_web.dart
+++ lib/web/purchases_flutter_web.dart
@@ -12,7 +12,7 @@ import '../purchases_flutter.dart';
 class PurchasesFlutterPlugin {
   static final _unknownErrorCode = '${PurchasesErrorCode.unknownError.index}';
   static final _configurationErrorCode = '${PurchasesErrorCode.configurationError.index}';
-  static const _purchasesHybridMappingsVersion = '16.0.2';
+  static const _purchasesHybridMappingsVersion = '16.1.0';
   static const _platformName = 'flutter';
   static const _pluginVersion = '9.1.0';
   static const _purchasesHybridMappingsUrl =
