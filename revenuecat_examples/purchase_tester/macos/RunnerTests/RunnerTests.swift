4d89801707cc53bd85bf652fab6158b92b8204f6
Antonio Pallares <ajpallares@users.noreply.github.com>
2025-09-05 13:48:40 +0200

    Default to Swift Package Manager in Purchase Tester (instead of CocoaPods) (#1454)
    This PR removes all the traces of CocoaPods from the Purchase Tester
    example app of the repo. This way, purchases_flutter is added as a local
    dependency via Swift Package Manager.
    
    The Purchase Tester project is also ready to seamlessly work with
    CocoaPods for development when needed. To do that, just run
    ```bash
    flutter config --no-enable-swift-package-manager
    ```
    at the beginning of the session.
    
    To re-enable Swift Package Manager, run 
    ```bash
    flutter config --enable-swift-package-manager
    ```
    
    ## Notes for reviewer
    The changes in this PR are:
    * Remove all traces of CocoaPods from the Purchase Tester example app
    * Commit all file changes generated by flutter when doing `flutter pub
    get` and `flutter build ios (macos)` when using SPM

---
+++ revenuecat_examples/purchase_tester/macos/RunnerTests/RunnerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunnerTests.swift
+//  RunnerTests
+//
+//  Created by Antonio Pallares on 5/9/25.
+//  Copyright © 2025 The Flutter Authors. All rights reserved.
+//
+
+import Testing
+
+struct RunnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
