165264e5076c315f6c854d880cc862a8b0d4dd3f
RCGitBot <dev+RCGitBot@revenuecat.com>
2025-09-15 22:47:14 +0000

    Version bump for 9.6.1

--- ios/purchases_flutter/Sources/purchases_flutter/PurchasesFlutterPlugin.m
+++ ios/purchases_flutter/Sources/purchases_flutter/PurchasesFlutterPlugin.m
@@ -787,7 +787,7 @@ readyForPromotedProduct:(RCStoreProduct *)product
 }
 
 - (NSString *)platformFlavorVersion {
-    return @"9.6.0";
+    return @"9.6.1";
 }
 
 - (NSError *)createUnsupportedErrorWithDescription:(NSString *)description {
