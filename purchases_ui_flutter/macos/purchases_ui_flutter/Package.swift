6e31347205c20cf62c819d21733418100131a058
RCGitBot <dev+RCGitBot@revenuecat.com>
2025-11-20 20:43:47 +0000

    Version bump for 17.19.1

--- purchases_ui_flutter/macos/purchases_ui_flutter/Package.swift
+++ purchases_ui_flutter/macos/purchases_ui_flutter/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "purchases-ui-flutter", targets: ["purchases_ui_flutter"])
     ],
     dependencies: [
-        .package(url: "https://github.com/RevenueCat/purchases-hybrid-common.git", exact: "17.19.0")
+        .package(url: "https://github.com/RevenueCat/purchases-hybrid-common.git", exact: "17.19.1")
     ],
     targets: [
         .target(
