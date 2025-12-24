4a85bc4223c142ee1a989ab3f43f4d53ef38feb6
RCGitBot <dev+RCGitBot@revenuecat.com>
2025-12-24 17:35:20 +0000

    Version bump for 9.10.2

--- lib/web/purchases_flutter_web.dart
+++ lib/web/purchases_flutter_web.dart
@@ -14,7 +14,7 @@ class PurchasesFlutterPlugin {
   static final _configurationErrorCode = '${PurchasesErrorCode.configurationError.index}';
   static const _purchasesHybridMappingsVersion = '17.25.0';
   static const _platformName = 'flutter';
-  static const _pluginVersion = '9.10.1';
+  static const _pluginVersion = '9.10.2';
   static const _purchasesHybridMappingsUrl =
       'https://cdn.jsdelivr.net/npm/@revenuecat/purchases-js-hybrid-mappings@$_purchasesHybridMappingsVersion/dist/index.umd.js';
 
