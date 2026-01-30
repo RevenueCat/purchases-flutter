bdc640084f3080047bcfd8bdd32e1b8319354e98
RCGitBot <dev+RCGitBot@revenuecat.com>
2026-01-30 11:43:12 +0000

    Version bump for 17.30.2

--- macos/purchases_flutter/Package.swift
+++ macos/purchases_flutter/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "purchases-flutter", targets: ["purchases_flutter"])
     ],
     dependencies: [
-        .package(url: "https://github.com/RevenueCat/purchases-hybrid-common.git", exact: "17.30.0")
+        .package(url: "https://github.com/RevenueCat/purchases-hybrid-common.git", exact: "17.30.2")
     ],
     targets: [
         .target(
