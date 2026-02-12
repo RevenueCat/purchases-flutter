0732c963e4cb306fb60ddcb74df3f73a91b5c8d8
Facundo Menzella <facumenzella@users.noreply.github.com>
2026-02-12 16:49:51 +0100

    Apply suggestions from code review

--- revenuecat_examples/purchase_tester/lib/src/constant.dart
+++ revenuecat_examples/purchase_tester/lib/src/constant.dart
@@ -1,5 +1,5 @@
 //TO DO: add the Apple API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
-const appleApiKey = 'appl_SYkbbYptqjDadVkQeBUtXxVxCcw';
+const appleApiKey = 'appl_api_key';
 
 //TO DO: add the Google API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
 const googleApiKey = 'goog_TzjuPFWcFelhzMSOGvKoZWvrdcW';
