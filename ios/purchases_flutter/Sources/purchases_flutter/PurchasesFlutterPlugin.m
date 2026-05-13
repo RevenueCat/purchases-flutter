2d65a2368bff07f8d7e51a0dc4d3862bde3e827b
RCGitBot <dev+RCGitBot@revenuecat.com>
2026-05-13 18:25:28 +0000

    Version bump for 10.1.0

--- ios/purchases_flutter/Sources/purchases_flutter/PurchasesFlutterPlugin.m
+++ ios/purchases_flutter/Sources/purchases_flutter/PurchasesFlutterPlugin.m
@@ -839,7 +839,7 @@ readyForPromotedProduct:(RCStoreProduct *)product
 }
 
 - (NSString *)platformFlavorVersion {
-    return @"10.0.2";
+    return @"10.1.0";
 }
 
 - (NSError *)createUnsupportedErrorWithDescription:(NSString *)description {
