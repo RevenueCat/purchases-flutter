dbd5d132e2653a869397eb3f0eb692bacf02c482
Peter Porfy <peter.porfy@revenuecat.com>
2026-08-10 16:15:31 +0200

    chore(ads): clear macos changes

--- revenuecat_examples/purchase_tester/macos/Flutter/GeneratedPluginRegistrant.swift
+++ revenuecat_examples/purchase_tester/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,8 @@ import Foundation
 
 import app_links
 import purchases_flutter
-import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
-  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
