b766595850d65e045a61c15a2b35d6a5f31db941
Álvaro Brey <alvaro.brey@revenuecat.com>
2026-09-14 12:57:41 +0200

    Forward paywall interactions from the native views

--- purchases_ui_flutter/ios/purchases_ui_flutter/Sources/purchases_ui_flutter/PurchasesUiPaywallView.swift
+++ purchases_ui_flutter/ios/purchases_ui_flutter/Sources/purchases_ui_flutter/PurchasesUiPaywallView.swift
@@ -172,4 +172,9 @@ extension PurchasesUiPaywallView: PaywallViewControllerDelegateWrapper {
     func paywallViewController(_ controller: PaywallViewController, didOpenURL url: String) {
         _methodChannel.invokeMethod("onUrlOpened", arguments: ["url": url])
     }
+
+    func paywallViewController(_ controller: PaywallViewController,
+                               didTrackInteraction eventDictionary: [String: Any]) {
+        _methodChannel.invokeMethod("onInteraction", arguments: eventDictionary)
+    }
 }
