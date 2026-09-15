66deac6a420a4476d87c0a1291205f23ac8c2b26
Álvaro Brey <alvaro.brey@revenuecat.com>
2026-09-15 11:03:03 +0200

    Deliver onPurchaseCompleted when there is no store transaction

--- purchases_ui_flutter/lib/views/paywall_view_method_handler.dart
+++ purchases_ui_flutter/lib/views/paywall_view_method_handler.dart
@@ -106,9 +106,12 @@ class PaywallViewMethodHandler {
     final customerInfo = CustomerInfo.fromJson(
       Map<String, dynamic>.from(arguments['customerInfo']),
     );
-    final storeTransaction = StoreTransaction.fromJson(
-      Map<String, dynamic>.from(arguments['storeTransaction']),
-    );
+    // PaywallProxy forwards `transaction?.dictionary`, so purchases completed without a
+    // StoreKit transaction arrive without one.
+    final transactionJson = arguments['storeTransaction'];
+    final storeTransaction = transactionJson == null
+        ? const StoreTransaction('', '', '')
+        : StoreTransaction.fromJson(Map<String, dynamic>.from(transactionJson));
     onPurchaseCompleted?.call(customerInfo, storeTransaction);
   }
 
--- purchases_ui_flutter/test/paywall_view_method_handler_test.dart
+++ purchases_ui_flutter/test/paywall_view_method_handler_test.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:purchases_flutter/models/customer_info_wrapper.dart';
+import 'package:purchases_flutter/models/store_transaction.dart';
 import 'package:purchases_ui_flutter/views/paywall_view_method_handler.dart';
+
+const _customerInfo = {
+  'entitlements': {'all': {}, 'active': {}},
+  'allPurchaseDates': {},
+  'allExpirationDates': {},
+  'activeSubscriptions': <String>[],
+  'allPurchasedProductIdentifiers': <String>[],
+  'nonSubscriptionTransactions': <dynamic>[],
+  'firstSeen': '2026-01-01T00:00:00Z',
+  'originalAppUserId': 'user',
+  'requestDate': '2026-01-01T00:00:00Z',
+  'managementURL': null,
+  'originalPurchaseDate': null,
+  'latestExpirationDate': null,
+  'originalApplicationVersion': null,
+};
 
 void main() {
   test('onInteraction receives the event map', () async {
@@ -29,5 +47,32 @@ void main() {
       'component_value': 'yearly',
       'paywall_revision': 3,
     });
+  });
+
+  test('onPurchaseCompleted fires when there is no store transaction', () async {
+    CustomerInfo? receivedCustomerInfo;
+    StoreTransaction? receivedTransaction;
+    final handler = PaywallViewMethodHandler(
+      null,
+      (customerInfo, storeTransaction) {
+        receivedCustomerInfo = customerInfo;
+        receivedTransaction = storeTransaction;
+      },
+      null,
+      null,
+      null,
+      null,
+      null,
+    );
+
+    await handler.handleMethodCall(
+      const MethodCall('onPurchaseCompleted', {
+        'customerInfo': _customerInfo,
+        'storeTransaction': null,
+      }),
+    );
+
+    expect(receivedCustomerInfo?.originalAppUserId, 'user');
+    expect(receivedTransaction, const StoreTransaction('', '', ''));
   });
 }
